238ab0f92ac992052d12499ea12bfb5d062abf39
silver-birch-wawa <3193151055@qq.com>
2019-08-26 22:40:39 +0800

    Site updated: 2019-08-26 22:40:38

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -4,13 +4,11 @@ copy_count=0;
 $('figure.highlight.plain .code pre').each(function(){
     var my=$(this);
     var father=$(this).parent('figure')
-    console.log(my);
     my.parent().prev().before("<button style='width:105%' class='btn-cp"+copy_count+"'>Copy</button>");
     var mycount=copy_count;
     copy_count+=1;
 
     var text=my['context']['innerText'];
-    console.log();
     var clipboard = new Clipboard('.btn-cp'+mycount, {
         // 点击copy按钮，直接通过text直接返回复印的内容
             text: function() {
@@ -20,16 +18,24 @@ $('figure.highlight.plain .code pre').each(function(){
     
 })
 // 保护知识产权
-if(document.domain!='localhost'){
-    var copyright=$(".copyright")[0]['innerText'];
-    $(document.body).bind({  
+if(document.domain=='localhost'){
+    var copyright="";
+    // 如果在首页无法找到copyright的话
+    if(window.location.protocol + '//' + window.location.host+'/'!=window.document.location.href){
+        copyright=$(".copyright")[0]['innerText'];
+    } 
+    else{
+        copyright="文章作者:SaltedMdFiveSh\r\n\r\n作者链接:https://silver-birch-wawa.github.io/\r\n\r\n许可协议: '署名-非商用-相同方式共享 4.0' 转载请保留原文链接及作者。"
+    }
+    $(document).bind({  
         copy: function(e) {//copy事件 
             var cpTxt =copyright; 
             var clipboardData = window.clipboardData; //for IE  
             if (!clipboardData) { // for chrome  
                 clipboardData = e.originalEvent.clipboardData;  
-            }  
-
+            }
+            console.log("copy success.");
+        
             //替换
             var selection = window.getSelection().toString();
             if(selection.length<15){
